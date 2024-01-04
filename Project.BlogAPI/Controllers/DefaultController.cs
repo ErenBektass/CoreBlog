3409999fbc9f18a2e37abbacfc53ff2b6e815ec1
Eren Bektaş <erenbektas95@hotmail.com>
2024-01-04 17:11:03 +0300

    Yorum satırları eklendi.

--- Project.BlogAPI/Controllers/DefaultController.cs
+++ Project.BlogAPI/Controllers/DefaultController.cs
@@ -4,11 +4,17 @@ using Project.BlogAPI.DAL;
 
 namespace Project.BlogAPI.Controllers
 {
-
+    /// <summary>
+    /// Bu controller kullanıcılarla olan işlemleri yapılmasını sağlar
+    /// </summary>
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class DefaultController : ControllerBase
     {
+        /// <summary>
+        /// Bu metot ile kullanıcıların listesini çekebilmektedir
+        /// </summary>
+        /// <returns> Kullanıcı listesi</returns>
         [HttpGet]
         public IActionResult EmployeeList()
         {
@@ -16,7 +22,12 @@ namespace Project.BlogAPI.Controllers
             var values = c.Employees.ToList();
             return Ok(values);
         }
-        [HttpPost]
+		/// <summary>
+		/// Bu metot  kullanıcıları ekler
+		/// </summary>
+		/// <param name="employee"> employee objesi gönderilir kulla </param>
+		/// <returns>Kullanıcı ekleme</returns>
+		[HttpPost]
         public IActionResult EmployeeAdd(Employee employee)
         {
             using var c = new Context();
@@ -24,7 +35,12 @@ namespace Project.BlogAPI.Controllers
             c.SaveChanges();
             return Ok();
         }
-        [HttpGet("{id}")]
+		/// <summary>
+		///  Bu metot  kullanıcıyı Getirir
+		/// </summary>
+		/// <param name="id"> Kullanıcının ID bilgisi Getirir </param>
+		/// <returns>Kullanıcı Getir</returns>
+		[HttpGet("{id}")]
         public IActionResult EmployeeGet(int id)
         {
             using var c = new Context();
@@ -38,7 +54,12 @@ namespace Project.BlogAPI.Controllers
                 return Ok(employee);
             }
         }
-        [HttpDelete("{id}")]
+		/// <summary>
+		/// Bu metot kullanıcıları siler
+		/// </summary>
+		/// <param name="id"> Kullanıcının ID'sine göre işlem yapar </param>
+		/// <returns>Kullanıcı Sil</returns>
+		[HttpDelete("{id}")]
         public IActionResult EmployeeDelete(int id)
         {
             using var c = new Context();
@@ -54,7 +75,12 @@ namespace Project.BlogAPI.Controllers
                 return Ok();
             }
         }
-        [HttpPut]
+		/// <summary>
+		/// Bu metot kullanıcıları Günceller
+		/// </summary>
+		/// <param name="id">Kullanıcıyı Güncelle </param>
+		/// <returns> Kullanıcı Güncelle</returns>
+		[HttpPut]
         public IActionResult EmployeeUpdate(Employee employee)
         {
             using var c = new Context();
--- Project.BlogAPI/Program.cs
+++ Project.BlogAPI/Program.cs
@@ -1,11 +1,43 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
+
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Swagger Documentation Section
+var info = new OpenApiInfo()
+{
+	Title = "BlogAPI Dökümantasyonu",
+	Version = "v1",
+	Description = "Kullanýcýlarýn bloglarý incelebildigi yorum yapabildigi proje",
+	Contact = new OpenApiContact()
+	{
+		Name = "Eren",
+		Email = "erenbektas95@hotmail.com",
+	}
+
+};
+
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", info);
+
+	// Set the comments path for the Swagger JSON and UI.
+	var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+	c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
